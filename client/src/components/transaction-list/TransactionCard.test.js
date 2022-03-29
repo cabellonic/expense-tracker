24bb4e6553fbc5c6d735f1e4da0b226b850bc4b5
Nicolás Cabello <cabellonic@gmail.com>
2022-03-28 23:32:58 -0300

    fix: Description field is now called 'note' in the dummy data

--- client/src/components/transaction-list/TransactionCard.test.js
+++ client/src/components/transaction-list/TransactionCard.test.js
@@ -12,21 +12,21 @@ test("renders content", () => {
 	const item = {
 		id: "1",
 		title: "A transaction title",
-		description: "The description of the transaction",
+		note: "The note of the transaction",
 		amount: "100",
 		date: "2020-01-01",
 		category: "Shopping",
 		type: "expense",
 	};
 	const component = render(<TransactionCard item={item} />);
-	const formattedCurrency = getCurrency(item.amount);
+	const formattedAmount = getCurrency(item.amount);
 
 	expect(component.container).toHaveTextContent(item.title);
-	expect(component.container).toHaveTextContent(formattedCurrency);
+	expect(component.container).toHaveTextContent(formattedAmount);
 	expect(component.container.querySelector("span")).toHaveAttribute(
 		"data-category",
 		item.category
 	);
-	expect(component.getByText(formattedCurrency)).toHaveClass(item.type);
-	expect(component.getByText(formattedCurrency)).not.toHaveClass("income");
+	expect(component.getByText(formattedAmount)).toHaveClass(item.type);
+	expect(component.getByText(formattedAmount)).not.toHaveClass("income");
 });
