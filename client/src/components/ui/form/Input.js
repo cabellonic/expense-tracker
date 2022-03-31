3b17cd9adf92a5a9aac219345bb74475e2de351e
Nicolás Cabello <cabellonic@gmail.com>
2022-03-31 18:34:05 -0300

    fix: Input component now have the correct type attribute

--- client/src/components/ui/form/Input.js
+++ client/src/components/ui/form/Input.js
@@ -1,13 +1,14 @@
 // Styles
 import styles from "./Input.module.css";
 
-const Input = ({ register, error, placeholder }) => {
+const Input = ({ register, error, type, placeholder, ...props }) => {
 	return (
 		<input
 			className={`${styles.input} ${error ? styles.error : ""}`}
-			type="text"
+			type={type}
 			placeholder={placeholder}
 			{...register}
+			{...props}
 		/>
 	);
 };
