719e888bd9faf1c460abeb5a38b670f0eee8ac4d
Nicolás Cabello <cabellonic@gmail.com>
2022-04-01 16:00:14 -0300

    feat: Authenticated users can create transactions

---
+++ client/src/components/transaction-form/AddTransactionForm.js
@@ -0,0 +1,87 @@
+import { useContext } from "react";
+import { useForm } from "react-hook-form";
+// Components
+import Form from "components/ui/form/Form";
+import AmountInput from "./form-elements/AmountInput";
+import TypeSelector from "./form-elements/TypeSelector";
+import Input from "components/ui/form/Input";
+import CategorySelector from "./form-elements/CategorySelector";
+import Fieldset from "components/ui/form/Fieldset";
+import Button from "components/ui/Button";
+// Context
+import { AuthContext } from "context/AuthContext";
+
+const AddTransactionForm = ({ type = "expense" }) => {
+	const { userToken } = useContext(AuthContext);
+	const {
+		register,
+		handleSubmit,
+		setValue,
+		formState: { errors },
+	} = useForm({ mode: "onChange" });
+
+	const onSubmit = async (data) => {
+		const { amount, type, title, note, category } = data;
+		try {
+			const response = await fetch("http://localhost:5000/transactions", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+					amount,
+					type,
+					title,
+					note,
+					category,
+					token: userToken,
+				}),
+			});
+
+			const resData = await response.json();
+			// If credentials are invalid
+			// if (!resData.isLoggedIn) return setErrorMessage(resData.message);
+			console.log("RES DATA ADD TRANSACTION:", resData);
+			// login(resData.token);
+		} catch (err) {
+			// HANDLE ERROR LATER
+			console.log(err);
+		}
+	};
+
+	const amountRegister = register("amount", { required: true });
+	// const typeRegister = register("type", {
+	register("type", {
+		required: true,
+		value: type,
+	});
+	const titleRegister = register("title", {
+		required: true,
+		minLength: 3,
+		maxLength: 100,
+	});
+	const noteRegister = register("note");
+	const categoryRegister = register("category", {
+		required: true,
+	});
+
+	return (
+		<Form onSubmit={handleSubmit(onSubmit)}>
+			<AmountInput register={amountRegister} error={errors.amount} />
+			<TypeSelector setValue={setValue} type={type} />
+			<Input
+				register={titleRegister}
+				placeholder="Title"
+				error={errors.title}
+			/>
+			<Input register={noteRegister} placeholder="Note" error={errors.note} />
+			<CategorySelector register={categoryRegister} setValue={setValue} />
+
+			<Fieldset>
+				<Button type="submit">Add</Button>
+			</Fieldset>
+		</Form>
+	);
+};
+
+export default AddTransactionForm;
--- server/controllers/transactions.controller.js
+++ server/controllers/transactions.controller.js
@@ -1,4 +1,7 @@
+const jwt = require("jsonwebtoken");
 const { pool } = require("../db");
+const { config } = require("dotenv");
+config();
 
 exports.getAllTransactions = async (req, res) => {
 	res.status(200).json({ message: "All transactions" });
@@ -10,7 +13,36 @@ exports.getTransactionById = async (req, res) => {
 };
 
 exports.createTransaction = async (req, res) => {
-	res.status(200).json({ message: "Transaction created successfully" });
+	const { amount, title, note, type, category, token } = req.body;
+
+	// Get category id
+	const categoryId = await pool.query(
+		"SELECT id FROM category WHERE name = $1",
+		[category]
+	);
+	if (!categoryId.rowCount)
+		return res.status(400).json({ message: "Category not found" });
+
+	jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
+		if (err) {
+			return res.status(401).json({ message: "Invalid token" });
+		}
+
+		// The token is valid so we can create the transaction
+		const newTransaction = await pool.query(
+			"INSERT INTO transaction (amount, title, note, type, created_at, category_id, user_id) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *",
+			[amount, title, note, type, new Date(), categoryId.rows[0].id, decoded.id]
+		);
+
+		if (!newTransaction.rowCount) {
+			return res.status(400).json({ message: "Error" });
+		}
+
+		res.status(201).json({
+			message: "Transaction created",
+			transaction: newTransaction.rows[0],
+		});
+	});
 };
 
 exports.updateTransaction = async (req, res) => {
