24ae0b9beec4189878cbc125c192e4d49de56bb2
Nicolás Cabello <cabellonic@gmail.com>
2022-04-02 11:32:13 -0300

    fix: Show/Hide balance preference now persist when user refresh the page

--- client/src/layout/components/Balance.js
+++ client/src/layout/components/Balance.js
@@ -6,10 +6,15 @@ import { formatAmount } from "util/currency";
 import styles from "./Balance.module.css";
 
 const Balance = ({ amount = -93154 }) => {
-	const [showBalance, setShowBalance] = useState(true);
+	const [hideBalance, setHideBalance] = useState(
+		localStorage.getItem("hideBalance") === "true"
+	);
 
 	const toggleBalance = () => {
-		setShowBalance((prevState) => !prevState);
+		setHideBalance((prevState) => {
+			localStorage.setItem("hideBalance", !prevState);
+			return !prevState;
+		});
 	};
 
 	return (
@@ -17,19 +22,19 @@ const Balance = ({ amount = -93154 }) => {
 			<div className={styles.balance}>
 				<span className={styles.title}>Total balance</span>
 				<span className={styles.amount}>
-					{showBalance ? formatAmount(amount) : "$＊＊＊＊"}
+					{hideBalance ? "$＊＊＊＊" : formatAmount(amount)}
 
-					{showBalance ? (
+					{hideBalance ? (
 						<FontAwesomeIcon
 							className={styles.amount_toggle}
 							onClick={toggleBalance}
-							icon={["fas", "eye"]}
+							icon={["fas", "eye-slash"]}
 						/>
 					) : (
 						<FontAwesomeIcon
 							className={styles.amount_toggle}
 							onClick={toggleBalance}
-							icon={["fas", "eye-slash"]}
+							icon={["fas", "eye"]}
 						/>
 					)}
 				</span>
@@ -39,7 +44,7 @@ const Balance = ({ amount = -93154 }) => {
 					</span>
 					<div className={styles.balance_incomes}>
 						<span>Incomes</span>
-						<span>{showBalance ? formatAmount(5701) : "$＊＊＊"}</span>
+						<span>{hideBalance ? "$＊＊＊" : formatAmount(5701)}</span>
 					</div>
 				</div>
 				<div className={styles.balance_info}>
@@ -48,7 +53,7 @@ const Balance = ({ amount = -93154 }) => {
 					</span>
 					<div className={styles.balance_expenses}>
 						<span>Expenses</span>
-						<span>{showBalance ? formatAmount(-1504) : "$＊＊＊"}</span>
+						<span>{hideBalance ? "$＊＊＊" : formatAmount(-1504)}</span>
 					</div>
 				</div>
 			</div>
