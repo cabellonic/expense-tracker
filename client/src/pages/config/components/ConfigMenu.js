5b0bb43383bd5b06202444c2d2764fb97b689c2e
Nicolás Cabello <cabellonic@gmail.com>
2022-04-01 23:43:27 -0300

    update: Users can logout

--- client/src/pages/config/components/ConfigMenu.js
+++ client/src/pages/config/components/ConfigMenu.js
@@ -1,21 +1,34 @@
+import { useContext } from "react";
+import { navigate } from "@reach/router";
 import { Link } from "@reach/router";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// Context
+import { AuthContext } from "context/AuthContext";
 // Styles
 import styles from "./ConfigMenu.module.css";
 
 const ConfigMenu = () => {
+	const { logout } = useContext(AuthContext);
+
+	const handleLogout = () => {
+		logout();
+		// I'll send the user to the main page
+		// But the code already do that
+		navigate("/");
+	};
+
 	return (
 		<div className={styles.config_menu}>
-			<Link className={styles.link} to={"/"}>
+			<Link className={styles.item} to={"/"}>
 				<FontAwesomeIcon icon={["fas", "pen-to-square"]} /> Edit categories
 			</Link>
-			<Link className={styles.link} to={"/config/user"}>
+			<Link className={styles.item} to={"/config/user"}>
 				<FontAwesomeIcon icon={["fas", "user"]} /> Edit information
 			</Link>
-			<Link className={styles.link} to={"/"}>
+			<span onClick={handleLogout} className={styles.item} to={"/"}>
 				<FontAwesomeIcon icon={["fas", "arrow-right-from-bracket"]} /> Log out
-			</Link>
-			<Link className={styles.link + ` ${styles.danger}`} to={"/"}>
+			</span>
+			<Link className={styles.item + ` ${styles.danger}`} to={"/"}>
 				<FontAwesomeIcon icon={["fas", "trash-can"]} /> Delete account
 			</Link>
 		</div>
