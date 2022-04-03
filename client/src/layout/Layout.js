00f9867dfc513c1e31b91933d040f3f5cf48d7f8
Nicolás Cabello <cabellonic@gmail.com>
2022-04-03 18:35:31 -0300

    update: Added some meta tags

--- client/src/layout/Layout.js
+++ client/src/layout/Layout.js
@@ -9,12 +9,21 @@ import Footer from "./components/Footer";
 import styles from "./Layout.module.css";
 
 const Layout = ({ balance, menu, pageTitle, center, from, children }) => {
+	const description =
+		"Keep track of your expenses and see how much you can save";
+
 	return (
 		<main className={styles.layout}>
 			<Helmet
 				title={pageTitle === "Expense Tracker" ? "Welcome" : pageTitle}
 				titleTemplate={`%s | Expense Tracker`}
-			/>
+			>
+				<meta name="description" content={description} />
+				<meta name="og:description" content={description} />
+				<meta name="twitter:description" content={description} />
+				<meta property="og:image" content="/logo512.png" />
+				<meta property="twitter:image" content="/logo512.png" />
+			</Helmet>
 			<Header pageTitle={pageTitle} center={center} href={from} />
 			{balance && <Balance />}
 			{menu && <Menu />}
